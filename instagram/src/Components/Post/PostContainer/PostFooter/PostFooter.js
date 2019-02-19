8f202f49854e2f0e3fca19ba956815ef41c7c0bf
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-19 15:11:16 -0800

    Refactored PostLike Component Data Flow Structure

--- instagram/src/Components/Post/PostContainer/PostFooter/PostFooter.js
+++ instagram/src/Components/Post/PostContainer/PostFooter/PostFooter.js
@@ -4,14 +4,12 @@ import React from 'react';
 import './PostFooter.css';
 
 // Components
-import PostIcons from './PostIcons/PostIcons';
 import PostLikes from './PostLikes/PostLikes';
 import CommentSection from './CommentSection/CommentSection';
 
 const PostFooter = props => {
     return (
         <div className="post-footer">
-            <PostIcons />
             <PostLikes likes={props.likes} />
             <CommentSection comments={props.comments} timestamp={props.timestamp} addComment={props.addComment} />
         </div>
--- instagram/src/Components/Post/PostContainer/PostFooter/PostLikes/PostLikes.js
+++ instagram/src/Components/Post/PostContainer/PostFooter/PostLikes/PostLikes.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import PostIcons from './PostIcons/PostIcons';
 
 import './PostLikes.css';
 
@@ -6,6 +8,31 @@ function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-const PostLikes = props => <span className="post-likes">{numberWithCommas(props.likes)} likes</span>
+class PostLikes extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            likes: props.likes
+        }
+    }
+
+    likePost = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            likes: this.state.likes + 1,
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <PostIcons />
+                <span>{numberWithCommas(this.state.likes)} likes</span>
+            </div>
+        )
+    }
+}
 
 export default PostLikes;
