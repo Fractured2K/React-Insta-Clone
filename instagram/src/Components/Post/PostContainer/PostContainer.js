b8b4fec58321b2b556a902bb6715794e2912bbb9
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-21 12:41:02 -0800

    Refactored PostContainer Component to use styled components

--- instagram/src/Components/Post/PostContainer/PostContainer.js
+++ instagram/src/Components/Post/PostContainer/PostContainer.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 // Components
 import PostHeader from './PostHeader/PostHeader';
 import PostImage from './PostImage/PostImage';
 import PostFooter from './PostFooter/PostFooter';
 
-// Styles
-import './PostContainer.css';
+// Styled Components
+const SinglePostContainer = styled.div`
+    max-width: 614px;
+    width: 100%;
+    border-radius: 3px;
+    border: 1px solid #e6e6e6;
+    margin-top: 60px;
+    background: #FFFFFF;
+`;
 
 const PostContainer = props => {
     return (
-        <div className="post-container">
+        <SinglePostContainer>
             <PostHeader profileImage={props.post.thumbnailUrl} username={props.post.username} />
             <PostImage image={props.post.imageUrl} />
             <PostFooter likes={props.post.likes} comments={props.post.comments} timestamp={props.post.timestamp} />
-        </div>
+        </SinglePostContainer>
     )
 }
 
