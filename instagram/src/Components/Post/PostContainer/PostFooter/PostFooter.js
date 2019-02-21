1a3a8591e1bbf27afefdd0fc8e35f644768c0e0e
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-21 12:55:27 -0800

    Refactored PostFooter Component to use styled components

--- instagram/src/Components/Post/PostContainer/PostFooter/PostFooter.js
+++ instagram/src/Components/Post/PostContainer/PostFooter/PostFooter.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// Styles
-import './PostFooter.css';
+import styled from 'styled-components';
 
 // Components
 import PostLikes from './PostLikes/PostLikes';
 import CommentSection from './CommentSection/CommentSection';
 
+// Styled components
+const PostFooterContainer = styled.div`
+    margin-bottom: 5px;
+    padding: 0 16px;
+`;
+
+
 const PostFooter = props => {
     return (
-        <div className="post-footer">
+        <PostFooterContainer>
             <PostLikes likes={props.likes} />
             <CommentSection comments={props.comments} timestamp={props.timestamp} addComment={props.addComment} />
-        </div>
+        </PostFooterContainer>
     )
 }
 
