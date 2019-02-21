ee3cbd527c01c912d66d78a370f93768f628760a
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-21 13:15:33 -0800

    Refactored App Component to use styled components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './reset.css';
-import './App.css';
 
 // Dummy data
 import data from '../../instagram/src/dummy-data';
@@ -13,6 +14,11 @@ import Login from './Components/Login/Login';
 import ShowPosts from './Components/ShowPosts/ShowPosts';
 
 const Authentication = authenticate(Login)(ShowPosts);
+
+// Styled Components
+const AppContainer = styled.div`
+  background: #FAFAFA;
+`;
 
 class App extends Component {
   state = {
@@ -61,9 +67,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App" >
+      <AppContainer>
         <Authentication login={this.handleLogin} logout={this.handleLogout} posts={this.state.filteredPosts.length > 0 ? this.state.filteredPosts : this.state.posts} search={this.handleSearch} />
-      </div>
+      </AppContainer>
     );
   }
 }
