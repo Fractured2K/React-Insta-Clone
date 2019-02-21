f951692f5dfbbcdafa1c3612e7626f0eebd26f4f
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-21 13:04:59 -0800

    Refactored PostDate Component to use styled components

--- instagram/src/Components/Post/PostContainer/PostFooter/PostDate/PostDate.js
+++ instagram/src/Components/Post/PostContainer/PostFooter/PostDate/PostDate.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Moment from 'react-moment';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './PostDate.css';
+const PostTimestamp = styled.span`
+    font-size: 10px;
+    letter-spacing: .2px;
+    color: #999;
+    text-transform: uppercase;
+`;
 
-const PostDate = props => <span className="post-timestamp"><Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">{props.timestamp}</Moment></span>
+const PostDate = props => <PostTimestamp className="post-timestamp"><Moment fromNow parse="MMMM Do YYYY, hh:mm:ss a">{props.timestamp}</Moment></PostTimestamp>
 
 
 PostDate.propTypes = {
