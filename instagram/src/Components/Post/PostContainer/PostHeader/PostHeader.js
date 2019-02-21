23f0547a1ef392b9f475a71e4a0cce47992fdf08
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-02-21 12:49:50 -0800

    Refactored PostHeader Component to use styled components

--- instagram/src/Components/Post/PostContainer/PostHeader/PostHeader.js
+++ instagram/src/Components/Post/PostContainer/PostHeader/PostHeader.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './PostHeader.css';
+const PostHead = styled.div`
+    display: flex;
+    line-height: 30px;
+    padding: 16px;
+
+    img {
+        border-radius: 50%;
+        width: 30px;
+        height: 30px;
+    }
+
+    span {
+        font-size: 14px;
+        font-weight: 600;
+        margin: 0 8px;
+    }
+`;
 
 const PostHeader = props => {
     return (
-        <div className="post-header">
+        <PostHead>
             <img src={props.profileImage} alt="" />
 
             <span>{props.username}</span>
-        </div>
+        </PostHead>
     )
 }
 
